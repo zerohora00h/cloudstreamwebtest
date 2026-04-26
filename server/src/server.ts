682e4b368e429d66cca739f18952e173575eaaa7
Railson OL <70449512+zerohora00h@users.noreply.github.com>
2026-04-26 17:30:18 -0300

    Revert "Fix: Add missing fs import in server.ts"
    This reverts commit d18e7c09bc5862771d2fd7982d13c51919bc5592.

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,7 +12,6 @@ moduleAlias.addAliases({
 
 import cors from 'cors';
 import express from 'express';
-import fs from 'fs';
 import os from 'os';
 import { PluginRegistry } from './core/pluginRegistry';
 import { pluginRoutes } from './routes/plugins';
